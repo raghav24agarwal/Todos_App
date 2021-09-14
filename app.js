b73825ca6fa99d0a5812fe93853494509483fb51
Raghav Agarwal <rsag732@gmail.com>
2021-09-14 21:08:14 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input__id");
+const todos = document.getElementById("todos__id");
+
+const getTodos = JSON.parse(localStorage.getItem("todos"));
+
+if(getTodos) {
+    getTodos.forEach(getTodo => {
+        addTodo(getTodo)
+    })
+}
+
+form.addEventListener("submit" , (e) => {
+    e.preventDefault();
+
+    addTodo();
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if(todo) {
+        todoText = todo.text;
+    }
+
+    if(todoText) {
+
+        const todoEl = document.createElement("li");
+
+        if (todo && todo.completed) {
+            todoEl.classList.add('completed');
+
+        }
+
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener('click' , () => {
+            todoEl.classList.toggle('completed')
+
+            updateLS();
+        });
+
+        todoEl.addEventListener('contextmenu' , (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLS();
+        })
+
+        todos.appendChild(todoEl);
+
+        input.value = '';
+
+        updateLS();
+    }
+}
+
+function updateLS() {
+
+    const todosEl = document.querySelectorAll("li");
+
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text : todoEl.innerText,
+            completed : todoEl.classList.contains("completed"),
+        });
+    });
+
+    localStorage.setItem("todos" , JSON.stringify(todos));
+}
